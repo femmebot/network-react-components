41e3c171c4bdabd94ff76ff177235ebc61023184
Josh Schwartzman <josh@dreamseedo.org>
2018-09-05 11:06:39 -0700

    Adds Coupon model

---
+++ api.network.v1/Coupon.ts
@@ -0,0 +1,21 @@
+import {Model, prop} from 'datx'
+import {jsonapi} from 'datx-jsonapi'
+import {DateTimeString} from '~shared/data';
+
+export class Coupon extends jsonapi(Model) {
+  public static type = 'coupons'
+
+  @prop.identifier public id: number
+
+  @prop public code: string
+
+  @prop public name: string
+
+  @prop public amount_off: number
+
+  @prop public percent_off: number
+
+  @prop public currency: string
+
+  @prop public expires_at: DateTimeString
+}
