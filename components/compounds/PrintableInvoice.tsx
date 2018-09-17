525ee728533ddd9c9e3d312c6c77959859486bb4
re5et <re5etsmyth@gmail.com>
2018-09-17 15:10:05 -0700

    Normalize field label usage for printable invoice

--- components/compounds/PrintableInvoice.tsx
+++ components/compounds/PrintableInvoice.tsx
@@ -1,18 +1,19 @@
-import * as React from 'react'
+import * as React from "react";
 
-import Grid from '@material-ui/core/Grid/Grid'
-import {partition, sortBy, sumBy} from 'lodash'
-import styled from 'styled-components'
-import logo from '~shared/images/logo.png'
-import {Coupon, Invoice, Subscription} from '~shared/api.network.v1'
-import {InvoiceItem} from '~shared/api.network.v1/InvoiceItem'
-import Box from '~shared/components/atoms/Box'
-import Center from '~shared/components/atoms/Center'
-import HorizontalDivider from '~shared/components/atoms/HorizontalDivider'
-import Typography from '~shared/components/atoms/Typography'
-import {colors} from '~shared/styles/index'
-import {pxToRem} from '~shared/styles/utils'
-import {formatAsDollarAmount, formatDate} from '~shared/utils/formatters'
+import Grid from "@material-ui/core/Grid/Grid";
+import { partition, sortBy, sumBy } from "lodash";
+import styled from "styled-components";
+import logo from "~shared/images/logo.png";
+import { Coupon, Invoice, Subscription } from "~shared/api.network.v1";
+import { InvoiceItem } from "~shared/api.network.v1/InvoiceItem";
+import Box from "~shared/components/atoms/Box";
+import Center from "~shared/components/atoms/Center";
+import HorizontalDivider from "~shared/components/atoms/HorizontalDivider";
+import Typography from "~shared/components/atoms/Typography";
+import FieldLabel from "~shared/components/atoms/FieldLabel";
+import { colors } from "~shared/styles/index";
+import { pxToRem } from "~shared/styles/utils";
+import { formatAsDollarAmount, formatDate } from "~shared/utils/formatters";
 
 const Wrapper = styled.div`
   padding: ${pxToRem(90)} ${pxToRem(20)};
@@ -22,49 +23,49 @@ const Wrapper = styled.div`
     max-width: 100%;
     padding: 0;
   }
-`
+`;
 const AddressItem = styled.div`
   line-height: 1;
-`
+`;
 
-const LabelValue: React.SFC<{label: string}> = ({label, children}) => (
+const LabelValue: React.SFC<{ label: string }> = ({ label, children }) => (
   <Grid item container alignItems="center">
     <Grid item xs={4}>
-      <Typography variant="label">{label}</Typography>
+      <FieldLabel>{label}</FieldLabel>
     </Grid>
     <Grid container item xs={8} justify="flex-end">
       <Typography variant="paragraph">{children}</Typography>
     </Grid>
   </Grid>
-)
+);
 
-const Subtotal: React.SFC<{items: InvoiceItem[]}> = ({items}) => (
+const Subtotal: React.SFC<{ items: InvoiceItem[] }> = ({ items }) => (
   <React.Fragment>
     <HorizontalDivider />
     <Grid container>
       <Grid item xs={6}>
-        <Typography variant="label">Subtotal</Typography>
+        <FieldLabel>Subtotal</FieldLabel>
       </Grid>
       <Grid item xs={6} container justify="flex-end">
         <Typography variant="paragraph">
-          <strong>{formatAsDollarAmount(sumBy(items, 'amount'))}</strong>
+          <strong>{formatAsDollarAmount(sumBy(items, "amount"))}</strong>
         </Typography>
       </Grid>
     </Grid>
   </React.Fragment>
-)
+);
 
-const CouponLine: React.SFC<{coupon: Coupon; invoice: Invoice}> = ({
+const CouponLine: React.SFC<{ coupon: Coupon; invoice: Invoice }> = ({
   coupon,
-  invoice,
+  invoice
 }) => {
   const discountAmount = coupon.amount_off
     ? coupon.amount_off
-    : invoice.amount * (coupon.percent_off / 100.0)
+    : invoice.amount * (coupon.percent_off / 100.0);
   return (
     <Grid container spacing={24}>
       <Grid item xs={10}>
-        <Typography variant="label">Promo Code Applied</Typography>
+        <FieldLabel>Promo Code Applied</FieldLabel>
       </Grid>
       <Grid item xs={2} container justify="flex-end">
         <Typography variant="paragraph">
@@ -72,34 +73,34 @@ const CouponLine: React.SFC<{coupon: Coupon; invoice: Invoice}> = ({
         </Typography>
       </Grid>
     </Grid>
-  )
-}
+  );
+};
 
 const guardedDivide = (amount: number, quantity: number) =>
-  quantity === 0 ? 0 : amount / quantity
+  quantity === 0 ? 0 : amount / quantity;
 
 interface Props {
-  invoice: Invoice
+  invoice: Invoice;
   organization: {
-    name_display: string
-  }
-  subscription: Subscription
+    name_display: string;
+  };
+  subscription: Subscription;
 }
 
 const PrintableInvoice: React.SFC<Props> = ({
   organization,
   invoice,
-  subscription,
+  subscription
 }) => {
-  const {coupon} = subscription
-  const [proRatedItems, items] = partition(invoice.invoice_items, 'prorated')
+  const { coupon } = subscription;
+  const [proRatedItems, items] = partition(invoice.invoice_items, "prorated");
   return (
     <Wrapper>
       <Center mb={40}>
         <Typography variant="heading-2">Billing Statement</Typography>
       </Center>
       <Box mb={15}>
-        <img src={logo} style={{width: pxToRem(64)}} />
+        <img src={logo} style={{ width: pxToRem(64) }} />
       </Box>
       <Box mb={50}>
         <Typography variant="paragraph">
@@ -117,22 +118,22 @@ const PrintableInvoice: React.SFC<Props> = ({
         <Grid item xs={5}>
           <LabelValue label="Customer">{organization.name_display}</LabelValue>
           <LabelValue label="Date Paid">
-            {formatDate(invoice.period_end, 'MM/DD/YYYY')}
+            {formatDate(invoice.period_end, "MM/DD/YYYY")}
           </LabelValue>
           <LabelValue label="Payment Method">
-            {invoice.payment_methods[0].brand} ending in{' '}
+            {invoice.payment_methods[0].brand} ending in{" "}
             {invoice.payment_methods[0].last4}
           </LabelValue>
         </Grid>
         <Grid item xs={2} />
         <Grid item xs={5}>
           <LabelValue label="Statement Date">
-            {formatDate(invoice.period_end, 'MM/DD/YYYY')}
+            {formatDate(invoice.period_end, "MM/DD/YYYY")}
           </LabelValue>
           <LabelValue label="Statement #">{invoice.id}</LabelValue>
           <LabelValue label="Billing Period">
-            {formatDate(invoice.period_start, 'MM/DD/YYYY')} –
-            {formatDate(invoice.period_end, 'MM/DD/YYYY')}
+            {formatDate(invoice.period_start, "MM/DD/YYYY")} –
+            {formatDate(invoice.period_end, "MM/DD/YYYY")}
           </LabelValue>
         </Grid>
       </Grid>
@@ -145,16 +146,16 @@ const PrintableInvoice: React.SFC<Props> = ({
         <React.Fragment>
           <Grid container spacing={24}>
             <Grid item xs={5}>
-              <Typography variant="label">Description</Typography>
+              <FieldLabel>Description</FieldLabel>
             </Grid>
             <Grid item xs={3} container justify="flex-end">
-              <Typography variant="label">Unit Price</Typography>
+              <FieldLabel>Unit Price</FieldLabel>
             </Grid>
             <Grid item xs={2} container justify="flex-end">
-              <Typography variant="label">Quantity</Typography>
+              <FieldLabel>Quantity</FieldLabel>
             </Grid>
             <Grid item xs={2} container justify="flex-end">
-              <Typography variant="label">Amount</Typography>
+              <FieldLabel>Amount</FieldLabel>
             </Grid>
           </Grid>
           {items.map(invoiceItem => (
@@ -192,16 +193,16 @@ const PrintableInvoice: React.SFC<Props> = ({
           <Box mt={50}>
             <Grid container spacing={24}>
               <Grid item xs={5}>
-                <Typography variant="label">Description</Typography>
+                <FieldLabel>Description</FieldLabel>
               </Grid>
               <Grid item xs={3} container justify="flex-end">
-                <Typography variant="label">Price Per User Day</Typography>
+                <FieldLabel>Price Per User Day</FieldLabel>
               </Grid>
               <Grid item xs={2} container justify="flex-end">
-                <Typography variant="label">User Days</Typography>
+                <FieldLabel>User Days</FieldLabel>
               </Grid>
               <Grid item xs={2} container justify="flex-end">
-                <Typography variant="label">Amount</Typography>
+                <FieldLabel>Amount</FieldLabel>
               </Grid>
             </Grid>
           </Box>
@@ -247,7 +248,7 @@ const PrintableInvoice: React.SFC<Props> = ({
       <Box mt={50}>
         <Grid container spacing={24}>
           <Grid item xs={10}>
-            <Typography variant="label">Total</Typography>
+            <FieldLabel>Total</FieldLabel>
           </Grid>
           <Grid item xs={2} container justify="flex-end">
             <Typography variant="paragraph">
@@ -276,7 +277,7 @@ const PrintableInvoice: React.SFC<Props> = ({
         </Typography>
       </Box>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default PrintableInvoice
+export default PrintableInvoice;
