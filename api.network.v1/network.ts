d321789d0d0f337e0806c501416888e14beff68b
re5et <re5etsmyth@gmail.com>
2018-10-03 15:56:19 -0700

    Update network setup to better deal with baseUrl

--- api.network.v1/network.ts
+++ api.network.v1/network.ts
@@ -1,8 +1,21 @@
-import {config} from 'datx-jsonapi'
 import IdeoSSO from 'ideo-sso'
-import ensureTrailingSlash from '~shared/utils/ensureTrailingSlash'
+import 'whatwg-fetch'
+import {config} from 'datx-jsonapi'
 
-config.baseUrl = ensureTrailingSlash(IdeoSSO.baseApiUrl)
+config.baseUrl = '/'
+
+const isNetworkUrl = (url: string) => url.indexOf(IdeoSSO.baseApiUrl) > -1
+
+config.fetchReference = (url: string, opts: any) => {
+  let requestUrl = url
+  if (isNetworkUrl(requestUrl)) {
+    // remove baseUrl / if it is at the start of a network url
+    // (happens when using utils saveModel / removeModel)
+    requestUrl = url.replace(/^\//, '')
+  }
+  return fetch(requestUrl, opts)
+}
+
 config.defaultFetchOptions = {
   credentials: 'include',
 }
