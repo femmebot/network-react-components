3a982e4a8215a3b918c5b7c85d5db8a87b7a0078
re5et <re5etsmyth@gmail.com>
2018-10-05 11:09:34 -0700

    Support showing component when no invoices

--- components/compounds/Invoices.tsx
+++ components/compounds/Invoices.tsx
@@ -1,74 +1,81 @@
-import { Grid } from "@material-ui/core";
-import * as React from "react";
-import styled from "styled-components";
-import DownloadIcon from "~shared/images/icon-download.svg";
-import { Invoice } from "~shared/api.network.v1";
-import Typography from "~shared/components/atoms/Typography";
-import FieldLabel from "~shared/components/atoms/FieldLabel";
-import Section from "~shared/components/molecules/Section";
-import { formatAsDollarAmount, formatDate } from "~shared/utils/formatters";
-import { colors } from "~shared/styles";
+import { Grid } from '@material-ui/core'
+import * as React from 'react'
+import styled from 'styled-components'
+import DownloadIcon from '~shared/images/icon-download.svg'
+import { Invoice } from '~shared/api.network.v1'
+import Typography from '~shared/components/atoms/Typography'
+import FieldLabel from '~shared/components/atoms/FieldLabel'
+import Section from '~shared/components/molecules/Section'
+import { formatAsDollarAmount, formatDate } from '~shared/utils/formatters'
+import { colors } from '~shared/styles'
 
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
-`;
+`
 
 const StyledStatus = styled.span`
   color: ${({ children }: { children: string }) =>
-    children === "Pending"
+    children === 'Pending'
       ? colors.silver
-      : children === "Failed"
+      : children === 'Failed'
         ? colors.error
-        : "inherit"};
-`;
+        : 'inherit'};
+`
 
 interface Props {
-  invoices: Invoice[];
+  invoices: Invoice[]
+  NoInvoicesComponent?: React.ComponentType
 }
 
-const Invoices: React.SFC<Props> = ({ invoices }) => (
+const Invoices: React.SFC<Props> = ({ invoices, NoInvoicesComponent }) => (
   <Section title="Billing Statements">
-    <Grid container spacing={40}>
-      <Grid item xs={5}>
-        <FieldLabel>Period</FieldLabel>
-      </Grid>
-      <Grid item xs={3}>
-        <FieldLabel>Total</FieldLabel>
-      </Grid>
-      <Grid item xs={3}>
-        <FieldLabel>Status</FieldLabel>
-      </Grid>
-      <Grid item xs={1} />
-    </Grid>
-    {invoices.map(invoice => (
-      <Typography variant="paragraph" key={`invoice-${invoice.id}`}>
+    {invoices.length > 0 ? (
+      <React.Fragment>
         <Grid container spacing={40}>
           <Grid item xs={5}>
-            {formatDate(invoice.period_start, "MM/DD/YYYY")}–
-            {formatDate(invoice.period_end, "MM/DD/YYYY")}
+            <FieldLabel>Period</FieldLabel>
           </Grid>
           <Grid item xs={3}>
-            {formatAsDollarAmount(invoice.amount)}
+            <FieldLabel>Total</FieldLabel>
           </Grid>
           <Grid item xs={3}>
-            <StyledStatus>
-              {invoice.status === "paid"
-                ? "Paid"
-                : invoice.status === "unpaid"
-                  ? "Pending"
-                  : "Failed"}
-            </StyledStatus>
+            <FieldLabel>Status</FieldLabel>
           </Grid>
-          <Grid item xs={1}>
-            <Link href={`/print/invoices/${invoice.id}`} target="_blank">
-              <DownloadIcon height={16} />
-            </Link>
-          </Grid>
+          <Grid item xs={1} />
         </Grid>
-      </Typography>
-    ))}
+        {invoices.map(invoice => (
+          <Typography variant="paragraph" key={`invoice-${invoice.id}`}>
+            <Grid container spacing={40}>
+              <Grid item xs={5}>
+                {formatDate(invoice.period_start, 'MM/DD/YYYY')}–
+                {formatDate(invoice.period_end, 'MM/DD/YYYY')}
+              </Grid>
+              <Grid item xs={3}>
+                {formatAsDollarAmount(invoice.amount)}
+              </Grid>
+              <Grid item xs={3}>
+                <StyledStatus>
+                  {invoice.status === 'paid'
+                    ? 'Paid'
+                    : invoice.status === 'unpaid'
+                      ? 'Pending'
+                      : 'Failed'}
+                </StyledStatus>
+              </Grid>
+              <Grid item xs={1}>
+                <Link href={`/print/invoices/${invoice.id}`} target="_blank">
+                  <DownloadIcon height={16} />
+                </Link>
+              </Grid>
+            </Grid>
+          </Typography>
+        ))}
+      </React.Fragment>
+    ) : NoInvoicesComponent ? (
+      <NoInvoicesComponent />
+    ) : null}
   </Section>
-);
+)
 
-export default Invoices;
+export default Invoices
