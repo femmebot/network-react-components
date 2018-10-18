f11829faf7a4123bdfd6e5bba988d9e79b1623c2
re5et <re5etsmyth@gmail.com>
2018-10-18 10:30:53 -0700

    Fix NetworkOrganization id property type

--- api.network.v1/NetworkOrganization.ts
+++ api.network.v1/NetworkOrganization.ts
@@ -1,18 +1,16 @@
 import BaseModel from '~shared/api.network.v1/BaseModel'
-import { prop } from 'datx'
-import { apiUrl } from '~shared/api.network.v1/util'
+import {prop} from 'datx'
+import {apiUrl} from '~shared/api.network.v1/util'
+import {NetworkOrganizationId} from '~shared/data'
 
 export class NetworkOrganization extends BaseModel {
   public static type = 'organizations'
 
   public static endpoint = apiUrl('organizations')
 
-  @prop.identifier
-  public id: number
+  @prop.identifier public id: NetworkOrganizationId
 
-  @prop
-  public name: string
+  @prop public name: string
 
-  @prop
-  public external_id: string
+  @prop public external_id: string
 }
