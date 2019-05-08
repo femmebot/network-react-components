419418743f978c229c30f43ece81960cb4e891c7
mtvillwock <mtvillwock@gmail.com>
2019-05-08 15:23:01 -0700

    Add in app purchase field to payment method UI
    https://trello.com/c/Thf4MtyU/1368-05-default-payment-methods-cover-in-app-purchases

--- components/compounds/PaymentMethodList.tsx
+++ components/compounds/PaymentMethodList.tsx
@@ -1,18 +1,19 @@
-import { Grid } from '@material-ui/core';
-import { isEmpty } from 'lodash';
-import * as React from 'react';
-import { PaymentMethod } from '~shared/api.network.v1';
-import FieldLabel from '~shared/components/atoms/FieldLabel';
-import PaymentMethodActionMenu from '~shared/components/compounds/PaymentMethodActionMenu';
-import CardExpirationField from '~shared/components/molecules/CardExpirationField';
-import CardNameField from '~shared/components/molecules/CardNameField';
-import CardStateField from '~shared/components/molecules/CardStateField';
+import { Grid } from '@material-ui/core'
+import { isEmpty } from 'lodash'
+import * as React from 'react'
+import { PaymentMethod } from '~shared/api.network.v1'
+import FieldLabel from '~shared/components/atoms/FieldLabel'
+import PaymentMethodActionMenu from '~shared/components/compounds/PaymentMethodActionMenu'
+import CardExpirationField from '~shared/components/molecules/CardExpirationField'
+import CardNameField from '~shared/components/molecules/CardNameField'
+import CardStateField from '~shared/components/molecules/CardStateField'
+import InAppPurchaseField from '~shared/components/molecules/InAppPurchaseField'
 
 export interface Props {
-  updatePaymentMethod: (paymentMethod: PaymentMethod) => Promise<void>;
-  makePaymentMethodDefault: (paymentMethod: PaymentMethod) => Promise<void>;
-  destroyPaymentMethod: (paymentMethod: PaymentMethod) => Promise<void>;
-  paymentMethods: PaymentMethod[];
+  updatePaymentMethod: (paymentMethod: PaymentMethod) => Promise<void>
+  makePaymentMethodDefault: (paymentMethod: PaymentMethod) => Promise<void>
+  destroyPaymentMethod: (paymentMethod: PaymentMethod) => Promise<void>
+  paymentMethods: PaymentMethod[]
 }
 
 const PaymentMethodList: React.SFC<Props> = ({
@@ -24,25 +25,31 @@ const PaymentMethodList: React.SFC<Props> = ({
   !isEmpty(paymentMethods) ? (
     <React.Fragment>
       <Grid container spacing={40}>
-        <Grid item xs={4}>
+        <Grid item xs={3}>
           <FieldLabel>Cards on file</FieldLabel>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={3}>
           <FieldLabel>Expiry</FieldLabel>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={3}>
+          <FieldLabel>In-App Purchases</FieldLabel>
+        </Grid>
+        <Grid item xs={3}>
           <FieldLabel>Status</FieldLabel>
         </Grid>
       </Grid>
       {paymentMethods.map(paymentMethod => (
         <Grid container spacing={40} key={paymentMethod.id}>
-          <Grid item xs={4}>
+          <Grid item xs={3}>
             <CardNameField paymentMethod={paymentMethod} />
           </Grid>
-          <Grid item xs={4}>
+          <Grid item xs={3}>
             <CardExpirationField paymentMethod={paymentMethod} />
           </Grid>
           <Grid item xs={3}>
+            <InAppPurchaseField paymentMethod={paymentMethod} />
+          </Grid>
+          <Grid item xs={2}>
             <CardStateField paymentMethod={paymentMethod} />
           </Grid>
           <Grid item xs={1}>
@@ -56,6 +63,6 @@ const PaymentMethodList: React.SFC<Props> = ({
         </Grid>
       ))}
     </React.Fragment>
-  ) : null;
+  ) : null
 
-export default PaymentMethodList;
+export default PaymentMethodList
