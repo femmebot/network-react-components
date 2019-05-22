a24cd83ddf936556c07611efa3aec28b69c66cf9
Josh Schwartzman <josh@dreamseedo.org>
2019-05-22 14:58:25 -0700

    Add Payment Method to Invoice Item

--- api.network.v1/InvoiceItem.ts
+++ api.network.v1/InvoiceItem.ts
@@ -1,27 +1,31 @@
-import BaseModel from '~shared/api.network.v1/BaseModel';
-import { prop } from 'datx';
-import { DateTimeString, InvoiceItemId } from '~shared/data';
-import { apiUrl } from '~shared/api.network.v1/util';
+import BaseModel from '~shared/api.network.v1/BaseModel'
+import { prop } from 'datx'
+import { DateTimeString, InvoiceItemId } from '~shared/data'
+import { apiUrl } from '~shared/api.network.v1/util'
+import { PaymentMethod } from '~shared/api.network.v1/PaymentMethod'
 
 export class InvoiceItem extends BaseModel {
-  public static type = 'invoice_items';
+  public static type = 'invoice_items'
 
-  public static endpoint = apiUrl('invoice_items');
+  public static endpoint = apiUrl('invoice_items')
 
   @prop.identifier
-  public id: InvoiceItemId;
+  public id: InvoiceItemId
   @prop
-  public description: string;
+  public description: string
   @prop
-  public quantity: number;
+  public quantity: number
   @prop
-  public amount: number;
+  public amount: number
   @prop
-  public unit_amount: number;
+  public unit_amount: number
   @prop
-  public period_start: DateTimeString;
+  public period_start: DateTimeString
   @prop
-  public period_end: DateTimeString;
+  public period_end: DateTimeString
   @prop
-  public prorated: boolean;
+  public prorated: boolean
+
+  @prop.toOne(PaymentMethod)
+  payment_method: PaymentMethod
 }
